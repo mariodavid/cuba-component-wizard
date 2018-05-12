465996341dc3fd55f2393ebcde11edbef9f95050
Mario <mario@die-davids.de>
2018-05-12 12:08:25 +0200

    explanation and further tries

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 CUBA component - Wizard
 ======================
+
+I tried different stuff in order to create a Wizard DSL.
+
+![wizard-component-result](https://github.com/mariodavid/cuba-component-wizard/blob/master/img/wizard-loader-result.png)
+
--- modules/gui/src/de/diedavids/cuba/wizard/gui/xml/layout/loaders/WizardLoader.java
+++ modules/gui/src/de/diedavids/cuba/wizard/gui/xml/layout/loaders/WizardLoader.java
@@ -1,7 +1,9 @@
 package de.diedavids.cuba.wizard.gui.xml.layout.loaders;
 
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.xml.layout.ComponentLoader;
 import com.haulmont.cuba.gui.xml.layout.loaders.ContainerLoader;
+import com.haulmont.cuba.gui.xml.layout.loaders.TabComponentLoader;
 import de.diedavids.cuba.wizard.gui.components.Wizard;
 import com.haulmont.bali.util.Dom4j;
 import com.haulmont.cuba.core.global.AppBeans;
@@ -10,71 +12,81 @@ import com.haulmont.cuba.gui.xml.layout.loaders.AbstractComponentLoader;
 import org.apache.commons.lang.StringUtils;
 import org.dom4j.Element;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WizardLoader extends ContainerLoader<Wizard> {
 
+    protected Map<Element, TabSheet.Tab> pendingLoadSteps = new LinkedHashMap<>();
 
     protected ComponentsFactory componentsFactory = AppBeans.get(ComponentsFactory.NAME);
 
+
+    /**
+     * basically I tried to copy some stuff from AccordionLoader.java but I'm not really sure
+     * if this is how it is meant to be used
+     *
+     */
     @Override
     public void createComponent() {
         resultComponent = factory.createComponent(Wizard.class);
         loadId(resultComponent, element);
 
-        List<Element> stepElements = Dom4j.elements(element, "step");
 
+        TabSheet tabSheet = componentsFactory.createComponent(TabSheet.class);
+        tabSheet.setSizeFull();
+
+
+        resultComponent.add(tabSheet);
+
+        List<Element> stepElements = element.elements("step");
         for (Element stepElement : stepElements) {
-            String stepScreen = stepElement.attributeValue("screen");
+            final String stepScreen = stepElement.attributeValue("screen");
+
+            ComponentLoader tabComponentLoader = getLoader(stepElement, TabComponentLoader.class);
+            TabSheet.Tab tab;
+
+            tabComponentLoader.createComponent();
+
+            Component tabComponent = tabComponentLoader.getResultComponent();
+
+            tab = tabSheet.addTab(stepScreen, tabComponent);
+
+            pendingLoadComponents.add(tabComponentLoader);
+
+            pendingLoadSteps.put(stepElement, tab);
+
+            /*
+            here the an XML frame tag is created in order to load that frame
+            Perhaps I need to create a custom Loader for a WizardStep like the TabComponentLoader?
+             */
             createElement(stepElement, stepScreen);
             createSubComponents(resultComponent, stepElement);
         }
 
     }
 
+    /**
+     * this is what I copied over from AccordionLoader as well, but I'm not really sure if this is necessary or what
+     * would need to be done here. In the example you did the heavy lifting here:
+     * https://github.com/cuba-labs/composite-panel-loader/blob/master/modules/gui/src/com/company/demo/gui/xml/layout/loaders/CompositePanelLoader.java#L26
+     *
+     * But when I tried that, it seems not to work in my case with the frames... but I might also be wrong.
+     */
     @Override
     public void loadComponent() {
-        resultComponent.setSizeFull();
-
-        TabSheet tabSheet = componentsFactory.createComponent(TabSheet.class);
-        tabSheet.setSizeFull();
-
-        resultComponent.add(tabSheet);
-
-        List<Element> stepElements = Dom4j.elements(element, "step");
-
-        int stepIndex = 0;
-        for (Element stepElement : stepElements) {
-            addStepToTabSheet(tabSheet, stepIndex, stepElement);
+        List<Element> tabElements = element.elements("step");
+        for (Element tabElement : tabElements) {
+            TabSheet.Tab tab = pendingLoadSteps.remove(tabElement);
+            if (tab != null) {
+                loadIcon(tab, tabElement);
+            }
         }
+
+        loadSubComponents();
     }
 
-    private void addStepToTabSheet(TabSheet tabSheet, int stepIndex, Element stepElement) {
-        String pageCaption = stepElement.attributeValue("caption");
-        String stepScreen = stepElement.attributeValue("screen");
-
-        if (StringUtils.isNotEmpty(pageCaption)) {
-
-
-            VBoxLayout tabContentWrapper = componentsFactory.createComponent(VBoxLayout.class);
-            tabContentWrapper.setSizeFull();
-
-            if (StringUtils.isNotEmpty(stepScreen)) {
-                //tabContent = loadTabContentFromFrame(stepScreen);
-                loadSubComponents();
-
-            }
-            else {
-                Component tabContent = null;
-                tabContent = createGenericTabContent(pageCaption);
-                tabContentWrapper.add(tabContent);
-
-            }
-
-            TabSheet.Tab tab = tabSheet.addTab("step" + stepIndex, tabContentWrapper);
-            tab.setCaption(pageCaption);
-        }
-    }
 
     private Element createElement(Element element, String stepScreen) {
         if (element.element("frame") == null) {
@@ -86,14 +98,4 @@ public class WizardLoader extends ContainerLoader<Wizard> {
         return element;
     }
 
-    private Component loadTabContentFromFrame(String frameScreenId) {
-        return getContext().getFrame().openFrame(resultComponent, frameScreenId);
-    }
-
-    private Component createGenericTabContent(String pageCaption) {
-        GroupBoxLayout tabContent = componentsFactory.createComponent(GroupBoxLayout.class);
-        tabContent.setCaption("Content " + pageCaption);
-        tabContent.setSizeFull();
-        return tabContent;
-    }
 }
