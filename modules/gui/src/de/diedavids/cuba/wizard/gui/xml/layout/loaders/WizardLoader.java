e4b292ae08a82d67d2e363fbd2d36455684643d2
Mario <mario@die-davids.de>
2018-05-16 17:02:44 +0200

    fixed with patch from Yuiry

--- modules/gui/src/de/diedavids/cuba/wizard/gui/xml/layout/loaders/WizardLoader.java
+++ modules/gui/src/de/diedavids/cuba/wizard/gui/xml/layout/loaders/WizardLoader.java
@@ -1,16 +1,12 @@
 package de.diedavids.cuba.wizard.gui.xml.layout.loaders;
 
-import com.haulmont.cuba.gui.components.*;
-import com.haulmont.cuba.gui.xml.layout.ComponentLoader;
-import com.haulmont.cuba.gui.xml.layout.loaders.ContainerLoader;
-import com.haulmont.cuba.gui.xml.layout.loaders.TabComponentLoader;
-import de.diedavids.cuba.wizard.gui.components.Wizard;
-import com.haulmont.bali.util.Dom4j;
 import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.TabSheet;
+import com.haulmont.cuba.gui.xml.layout.ComponentLoader;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
-import com.haulmont.cuba.gui.xml.layout.loaders.AbstractComponentLoader;
-import de.diedavids.cuba.wizard.gui.components.WizardStep;
-import org.apache.commons.lang.StringUtils;
+import com.haulmont.cuba.gui.xml.layout.loaders.ContainerLoader;
+import de.diedavids.cuba.wizard.gui.components.Wizard;
 import org.dom4j.Element;
 
 import java.util.LinkedHashMap;
@@ -23,7 +19,6 @@ public class WizardLoader extends ContainerLoader<Wizard> {
 
     protected ComponentsFactory componentsFactory = AppBeans.get(ComponentsFactory.NAME);
 
-
     @Override
     public void createComponent() {
         resultComponent = factory.createComponent(Wizard.class);
@@ -34,32 +29,29 @@ public class WizardLoader extends ContainerLoader<Wizard> {
         TabSheet tabSheet = componentsFactory.createComponent(TabSheet.class);
         tabSheet.setSizeFull();
 
-
         resultComponent.add(tabSheet);
 
         for (Element stepElement : stepElements) {
-            final String name = stepElement.attributeValue("caption");
+            String name = stepElement.attributeValue("caption");
 
             ComponentLoader stepComponentLoader = getLoader(stepElement, WizardStepLoader.class);
-            TabSheet.Tab tab;
             stepComponentLoader.createComponent();
 
-            Component.Container tabComponent = (Component.Container) stepComponentLoader.getResultComponent();
+            Component tabComponent = stepComponentLoader.getResultComponent();
 
-            tab = tabSheet.addTab(name, tabComponent);
+            TabSheet.Tab tab = tabSheet.addTab(name, tabComponent);
             tab.setCaption(name);
             pendingLoadComponents.add(stepComponentLoader);
 
             pendingLoadSteps.put(stepElement, tab);
-
         }
-
-        createSubComponents(resultComponent, element);
-
     }
 
     @Override
     public void loadComponent() {
+        loadWidth(resultComponent, element);
+        loadHeight(resultComponent, element);
+
         List<Element> tabElements = element.elements("step");
         for (Element tabElement : tabElements) {
             TabSheet.Tab tab = pendingLoadSteps.remove(tabElement);
@@ -68,8 +60,6 @@ public class WizardLoader extends ContainerLoader<Wizard> {
             }
         }
 
-
         loadSubComponents();
     }
-
 }
--- modules/gui/src/de/diedavids/cuba/wizard/gui/xml/layout/loaders/WizardStepLoader.java
+++ modules/gui/src/de/diedavids/cuba/wizard/gui/xml/layout/loaders/WizardStepLoader.java
@@ -6,17 +6,14 @@ import org.dom4j.Element;
 
 public class WizardStepLoader extends AbstractBoxLoader<WizardStep> {
 
-
     @Override
     public void createComponent() {
         resultComponent = (WizardStep) factory.createComponent(WizardStep.NAME);
         loadId(resultComponent, element);
 
-
         createElement(element, element.attributeValue("screen"));
         createSubComponents(resultComponent, element);
     }
-
 
     private Element createElement(Element element, String stepScreen) {
         if (element.element("frame") == null) {
@@ -27,5 +24,4 @@ public class WizardStepLoader extends AbstractBoxLoader<WizardStep> {
         }
         return element;
     }
-
 }
